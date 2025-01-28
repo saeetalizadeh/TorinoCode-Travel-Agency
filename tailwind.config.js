ba02ef0fbe5830daaa359341f2b0378dcf6596d2
Saeed Alizadeh <saeedalizadeh514980@gmail.com>
2025-01-29 00:03:38 +0330

    Project Created and some of configs for tailwind have been done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,57 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
+      container: {
+        center: true,
+        padding: {
+          xs: "1.088rem",
+          sm: "2.288rem",
+          md: "2.288rem",
+          lg: "3.988rem",
+          xl: "4.088rem",
+          "2xl": "7.875rem",
+        },
+      },
+      fontFamily: {
+        Vazir: "Vazir",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        colors: {
+          background: "#ffffff",
+          customGreen: {
+            100: "#D8FFE1",
+            200: "#28A745",
+            300: "#10411B",
+          },
+          customGray: {
+            100: "#282828",
+          },
+          customBlue: {
+            100: "#009ECA",
+          },
+          customYellow: {
+            100: "#D1B900",
+          },
+          customRed: {
+            100: "#D40000",
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "&>*");
+      addVariant("child-hover", "&>*:hover");
+    },
+  ],
 };
