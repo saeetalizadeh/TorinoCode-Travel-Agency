e459603216b56e7dd400b933bb2a3551dac2c156
Saeed Alizadeh <saeedalizadeh514980@gmail.com>
2025-01-30 20:59:54 +0330

    TanstackQuery installed and Interceptors created

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import "./globals.css";
 import localFont from "next/font/local";
+import TanstackQueryProvider from "@/components/partials/provider/TanstackQueryProvider";
 
 const Vazir = localFont({
   src: "./fonts/Vazir-Medium-FD.woff2",
@@ -10,8 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${Vazir.className}`}>
-        <Header />
-        {children}
+        <TanstackQueryProvider>
+          <Header />
+          {children}
+        </TanstackQueryProvider>
       </body>
     </html>
   );
