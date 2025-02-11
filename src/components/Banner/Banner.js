a90f7d3af0b2ac43f93d654146083001ec2d027f
Saeed Alizadeh <saeedalizadeh514980@gmail.com>
2025-02-11 23:36:24 +0330

    I found a bug in header dropDown and fixed it .

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative  h-[119px] w-full md:h-[350px]">
+    <div className="relative z-[-1] h-[119px] w-full md:h-[350px]">
       <Image src="/images/Untitled_design__1_.png" fill={true} alt="بنر" />
     </div>
   );
--- src/components/authForm/index.js
+++ src/components/authForm/index.js
@@ -33,7 +33,7 @@ function AuthForm() {
         <div className="relative delay-150">
           <div
             onClick={() => setDropDown((item) => !item)}
-            className="flex items-center gap-x-1 text-myGreen-200"
+            className="flex items-center gap-x-1 text-customGreen-200"
           >
             <div className="relative h-[14px] w-[14px] xs:h-5 xs:w-5 md:h-6 md:w-6">
               <Image fill={true} src="/images/Adamak.svg" alt="profile" />
@@ -43,24 +43,21 @@ function AuthForm() {
               {data?.data?.mobile}
             </span>
 
-            <div
-              className="
-            relative h-[14px] w-[14px] xs:h-5 xs:w-5 md:h-6 md:w-6"
-            >
+            <div className="relative h-[14px] w-[14px] xs:h-5 xs:w-5 md:h-6 md:w-6">
               <Image fill={true} src="/images/arrow-down.svg" alt="profile" />
             </div>
           </div>
-          {/* {dropDown && ( */}
+
           <div
-            className={`absolute -left-[10%] top-8 w-[167px] divide-y rounded-2xl bg-background  shadow-md xs:top-10 xs:w-[200px] md:top-10 mdC:top-10 lg:-left-[20%] lg:w-[246px] ${
-              dropDown ? "profile-down-active " : "profile-down-hidden"
+            className={`absolute -left-[10%] top-8 w-[167px] divide-y rounded-2xl bg-background shadow-md xs:top-10 xs:w-[200px] md:top-10 lg:-left-[20%] lg:w-[246px] ${
+              dropDown ? "profile-down-active" : "profile-down-hidden"
             }`}
           >
-            <div className="flex h-11 items-center gap-x-3 rounded-t-[11px] bg-myGray-105/80 px-3">
-              <div className="relative h-4 w-4 rounded-full bg-myGray-140 p-2 md:p-3">
+            <div className="flex h-11 items-center gap-x-3 rounded-t-[11px] bg-[#f4f4f4] px-3">
+              <div className="relative h-4 w-4 rounded-full bg-customGray-100/20 p-2 md:p-3">
                 <Image fill={true} src="/images/frame.svg" alt="profile" />
               </div>
-              <span className="w-fit text-[14px] font-medium text-myGreen-300 md:text-[16px] lg:text-[18px]">
+              <span className="w-fit text-[14px] font-medium text-customGreen-300 md:text-[16px] lg:text-[18px]">
                 {!!data?.data?.firstName
                   ? `${data.data.firstName} ${data.data.lastName}`
                   : data?.data?.mobile}
@@ -69,23 +66,22 @@ function AuthForm() {
 
             <Link
               href={"/profile"}
-              // onClick={() => setDropDown(false)}
-              className="relative flex h-[35px] items-center gap-x-2 pr-3 hover:bg-myGray-110 lg:h-[45px]"
+              className="relative flex h-[35px] items-center gap-x-2 pr-3 hover:bg-customGray-100/10 lg:h-[45px]"
             >
               <div className="relative h-4 w-4 md:h-5 md:w-5">
                 <Image fill={true} src="/images/profile.svg" alt="profile" />
               </div>
-              <span className="text-[12px] text-myGray-410 lg:text-[14px]">
+              <span className="text-[12px] text-customGray-100 lg:text-[14px]">
                 اطلاعات حساب کاربری
               </span>
             </Link>
 
-            <div className="flex h-[35px] items-center gap-x-2 rounded-b-[15px] pb-1.5 pr-3 hover:bg-myGray-110 lg:h-[45px]">
+            <div className="flex h-[35px] items-center gap-x-2 rounded-b-[15px] pb-1.5 pr-3 hover:cursor-pointer hover:bg-customGray-100/10 lg:h-[45px]">
               <div className="relative mt-1 h-4 w-4 md:h-5 md:w-5">
                 <Image fill={true} src="/images/logout.svg" alt="profile" />
               </div>
               <span
-                className="text-[12px] text-myRed-100 lg:text-[14px]"
+                className="text-[12px] text-customRed-100 lg:text-[14px]"
                 onClick={logOutHandler}
               >
                 خروج از حساب کاربری
@@ -96,16 +92,16 @@ function AuthForm() {
       ) : (
         <button
           onClick={() => setIsOpen(true)}
-          className=" border border-customGreen-200 rounded-lg xl:px-4 xl:py-2 px-2 py-1 "
+          className="rounded-lg border border-customGreen-200 px-2 py-1 xl:px-4 xl:py-2"
         >
-          <div className="md:flex gap-2 hidden">
-            <div className="relative w-5 h-5 xl:w-6 xl:h-6">
+          <div className="hidden gap-2 md:flex">
+            <div className="relative h-5 w-5 xl:h-6 xl:w-6">
               <Image src="/images/Adamak.svg" fill={true} alt="آدمک" />
             </div>
-            <span className="text-customGreen-200 xl:text-[18px] border-l pl-2 border-customGreen-200 text-sm">
+            <span className="border-l border-customGreen-200 pl-2 text-sm text-customGreen-200 xl:text-[18px]">
               ورود
             </span>
-            <span className="text-customGreen-200 xl:text-[18px] text-sm">
+            <span className="text-sm text-customGreen-200 xl:text-[18px]">
               ثبت نام
             </span>
           </div>
